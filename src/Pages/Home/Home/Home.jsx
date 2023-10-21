96ab840ee154b7e1df489da17062603f1ca33f0e
Zahid-saadi <zahidbinkashem@gmail.com>
2023-10-21 23:21:15 +0600

    helmet added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,10 +3,12 @@ import Category from '../../Category/Category';
 import About from '../About/About';
 import FoodMenu from '../../../Shared/FoodItems/FoodMenu/FoodMenu';
 import Review from '../Review/Review';
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
     return (
         <div className='bg-slate-800 '>
+            <Helmet> <title>Raw Relish Home</title> </Helmet>
             <Hero></Hero>
             <Category></Category>
             <About></About>
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import logo from '../../assets/logo3.png'
 const Navbar = () => {
     const navItem = (<>
            <li>
@@ -26,25 +25,13 @@ const Navbar = () => {
             <div className="navbar-start ">
                 <div className="dropdown underline">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h8m-8 6h16"
-                            />
-                        </svg>
+                       
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2">
                  {navItem}
                     </ul>
                 </div>
+                <img className=' h-16 w-16 p-2 mx-2' src={logo} alt="logo of raw relish" />
                 <a className="text-2xl font-extrabold text-yellow-700">Raw Relish</a>
             </div>
             <div className="navbar-center hidden lg:flex ">
