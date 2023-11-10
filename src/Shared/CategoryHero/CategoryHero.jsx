8082bb92c1842c31d4333ff3c2dee3d80a4835cf
Zahid-saadi <zahidbinkashem@gmail.com>
2023-11-10 06:30:56 +0600

    some color and style issue fixed on foodDisplay page

--- src/Shared/CategoryHero/CategoryHero.jsx
+++ src/Shared/CategoryHero/CategoryHero.jsx
@@ -5,10 +5,10 @@ const CategoryHero = ({ videoSource, heading }) => {
   
   
     return (
-      <div className="w-full h-fit p-20 pb-0  bg-gradient-to-r from-slate-900 to-black">
-        <h2 className='text-white h-20 mt-10 text-center font-bold text-4xl '>__{heading}__</h2>
+      <div className="w-full h-fit p-20 pb-0  bg-gradient-to-r from-slate-900 to-black hover:z-10">
+        <h2 className='text-white h-28  mt-10 p-6 text-center font-bold text-4xl bg-gradient-to-r bg-slate-500 from-slate-900 capitalize z-20 border-2 border-yellow-900   '>__{heading}__</h2>
         <video
-        className='w-screen object-cover h-[600px] border-2 border-b-0 border-yellow-900 '
+        className='w-screen object-cover h-[600px] border-2 border-b-0 border-t-0 border-yellow-900 '
         muted
           src={videoSource}
           loop={true}
--- src/Shared/FoodItems/FoodDisplay/FoodDisplay.jsx
+++ src/Shared/FoodItems/FoodDisplay/FoodDisplay.jsx
@@ -3,20 +3,29 @@ import React, { useState } from "react";
 const FoodDisplay = ({ item }) => {
     // console.log("From Offered Food", offeredItem);
     const { name, image, price, recipe, newPrice } = item;
+
+   
     // console.log(name, image, price, recipe);
 
     return (
-        <div style={{borderRadius: ' 200px 200px 200px 20px'}}
-            className="group h-72   w-full border-2 border-yellow-900 relative overflow-hidden hover:scale-110 duration-700 ">
+        <div className="group h-72   w-full relative overflow-hidden hover:scale-110 duration-700  ">
             <div>
-                <img src={image} alt="Offered Item" className="w-full h-72 overflow-hidden" />
+                <img src={image} alt="Offered Item" className="w-full h-72 overflow-hidden " />
             </div>
 
-            <div  className=" bg-black bg-opacity-90   absolute hidden  text-white p-4  w-full h-full top-0   group-hover:block ">
-                <div style={{borderRadius: ' 200px 200px 200px 20px'}} className="h-full w-full p-4 text-center flex flex-col justify-center content-center items-center  border-2 border-white delay-150  ">
+            <div className="  absolute hidden  text-white p-4  w-full h-full top-0   group-hover:block  ">
+                <div
+                    style={{ borderRadius: " 300px 200px 200px 20px" }}
+                    className=" bg-black bg-opacity-70  h-full w-full p-5 text-center flex flex-col justify-center content-center items-center  border-2 border-white delay-150  "
+                >
                     <h2 className="text-3xl m-3 text-gray-200">__{name}__</h2>
-                    <p className="">{recipe}</p>
-                    <p className=" text-yellow-500 p-2 "> price $ {price} </p>
+                    <p className="p-3">{recipe}</p>
+                    <p className=" text-yellow-500 p-2 font-medium "> price $ {price} </p>
+                    {
+                        newPrice && (
+                            <p className="font-bold text-yellow-300 text-lg">New Price: {newPrice}</p>
+                        )}
+                    
                 </div>
             </div>
         </div>
