6a72eab359475f85046a75f72aee53581b2546cc
Zahid-saadi <zahidbinkashem@gmail.com>
2023-12-09 08:05:24 +0600

    add headline bg img on items

--- src/Shared/CategoryHero/CategoryHero.jsx
+++ src/Shared/CategoryHero/CategoryHero.jsx
@@ -1,22 +1,22 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
+import headlinePhoto from "../../assets/woodTray.png";
 
 const CategoryHero = ({ videoSource, heading }) => {
-  // console.log(heading);
-  
-  
+    // console.log(heading);
+
     return (
-      <div className="w-full h-fit p-20 pb-0  bg-gradient-to-r from-cyan-950 to-black hover:z-10">
-        <h2 className='text-white h-28 w-full  mt-10 p-6 text-center font-bold text-4xl bg-gradient-to-bl bg-cyan-950 from-lime-950 capitalize z-20 '>__{heading}__</h2>
-        <video
-        className='w-screen object-cover h-[600px]  '
-        muted
-          src={videoSource}
-          loop={true}
-          autoPlay
-        
-        />
-        
-      </div>
+        <div className="w-full h-fit p-20 pb-0  bg-gradient-to-r from-cyan-950 to-black hover:z-10">
+            <div>
+                <div className="w-full">
+                    <figure>
+                        <img className="w-full h-40" src={headlinePhoto} alt="" />
+                    </figure>
+
+                    <h2 className="text-slate-950 py-8 -mt-28 text-center font-extrabold text-4xl capitalize z-20 ">__{heading}__</h2>
+                </div>
+            </div>
+            <video className="w-screen object-cover h-[600px]  " muted src={videoSource} loop={true} autoPlay />
+        </div>
     );
 };
 
