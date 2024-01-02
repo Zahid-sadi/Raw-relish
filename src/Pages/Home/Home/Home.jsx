8bf85246182479755fa2a9984374d93210d3e989
Zahid-saadi <zahidbinkashem@gmail.com>
2024-01-02 13:05:26 +0600

    seerviceType component added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -4,12 +4,14 @@ import About from '../About/About';
 import Review from '../Review/Review';
 import { Helmet } from 'react-helmet';
 import OfferedFood from '../../../Shared/FoodItems/OfferedFood/OfferedFood';
+import ServiceType from '../ServiceType/ServiceType';
 
 const Home = () => {
     return (
         <div className=''>
             <Helmet> <title>Raw Relish Home</title> </Helmet>
             <Hero></Hero>
+            <ServiceType></ServiceType>
             <Category></Category>
             <About></About>
             <OfferedFood></OfferedFood>
---
+++ src/Pages/Home/ServiceType/ServiceType.jsx
@@ -0,0 +1,41 @@
+import { FcCustomerSupport } from "react-icons/fc";
+import { MdDeliveryDining } from "react-icons/md";
+import { TbChefHat } from "react-icons/tb";
+import { FaBowlFood } from "react-icons/fa6";
+
+
+
+
+const ServiceType = () => {
+    return (
+        <div className="h-fit max-w-4xl mx-auto p-12 m-12 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-5">
+
+            <div className="w-full h-72 bg-transparent border hover:border-yellow-900 border-blue-500 space-y-4 text-center items-center justify-center hover:bg-yellow-600  hover:duration-1000 group">
+                <FcCustomerSupport className=" text-yellow-400 group-hover:text-black  h-16 w-16 text-center  my-5 mx-auto" />
+
+                <h3 className="text-blue-400 group-hover:text-gray-100 text-xl lg:text-3xl font-semibold lg:font-bold "> 24/7 Open</h3>
+                <p className="text-gray-400 group-hover:text-black">we are always open Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, unde.</p>
+            </div>
+            <div className="w-full h-72 bg-transparent border hover:border-yellow-900 border-blue-500 space-y-4 text-center items-center justify-center hover:bg-yellow-600  hover:duration-1000 group">
+                <MdDeliveryDining className=" text-yellow-400 group-hover:text-black h-16 w-16 text-center  my-5 mx-auto" />
+
+                <h3 className="text-blue-400 group-hover:text-gray-100 text-xl lg:text-3xl font-semibold lg:font-bold "> Home Delivery</h3>
+                <p className="text-gray-400 group-hover:text-black">we are always open Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, unde.</p>
+            </div>
+            <div className="w-full h-72 bg-transparent border hover:border-yellow-900 border-blue-500 space-y-4 text-center items-center justify-center hover:bg-yellow-600  hover:duration-1000 group">
+                <TbChefHat className=" text-yellow-400 group-hover:text-black  h-16 w-16 text-center  my-5 mx-auto" />
+
+                <h3 className="text-blue-400 group-hover:text-gray-100 text-xl lg:text-3xl font-semibold lg:font-bold ">Master Chef </h3>
+                <p className="text-gray-400 group-hover:text-black">we are always open Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, unde.</p>
+            </div>
+            <div className="w-full h-72 bg-transparent border hover:border-yellow-900 border-blue-500 space-y-4 text-center items-center justify-center hover:bg-yellow-600  hover:duration-1000 group">
+                <FaBowlFood className=" text-yellow-400 group-hover:text-black  h-16 w-16 text-center  my-5 mx-auto" />
+
+                <h3 className="text-blue-400 group-hover:text-gray-100 text-xl lg:text-3xl font-semibold lg:font-bold "> Safe Food </h3>
+                <p className="text-gray-400 group-hover:text-black">we are always open Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, unde.</p>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceType;
