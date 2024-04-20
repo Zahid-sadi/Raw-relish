a3f1cedc39667d9783efdd4a6e2105dc142c5820
Zahid-saadi <zahidbinkashem@gmail.com>
2024-04-20 20:16:58 +0600

    worked on category hero to solve sizing position and changed headline bg image

--- src/Shared/CategoryHero/CategoryHero.jsx
+++ src/Shared/CategoryHero/CategoryHero.jsx
@@ -1,21 +1,19 @@
-import React, { useRef, useState } from "react";
-import headlinePhoto from "../../assets/woodTray.png";
+// import React, { useRef, useState } from "react";
+import headlinePhoto from "../../assets/headingBg.png"
 
 const CategoryHero = ({ videoSource, heading }) => {
     // console.log(heading);
 
     return (
         <div className="w-full h-fit p-20 pb-0  bg-gradient-to-r from-cyan-950 to-black hover:z-10">
-            <div>
-                <div className="w-full mb-4">
-                    <img className="w-full h-40  " src={headlinePhoto} alt="" />
+                <div className="relative rounded-3xl rounded-b-0 border border-b-0 border-yellow-500  opacity-90 w-2/5 text-center mx-auto h-fit">
+                    <img className="w-full p-2 h-60" src={headlinePhoto} alt="" />
 
-                    <h2 className="text-slate-950 p-8 -mt-32 text-center font-bold lg:font-extrabold text-xl lg:text-4xl capitalize z-20 ">
-                        __{heading}__
+                    <h2 className="text-white text-center  absolute  bottom-0 inset-x-0 p-5  font-bold lg:font-bold text-sm md:text-lg lg:text-4xl capitalize   ">
+                    {heading}
                     </h2>
                 </div>
-            </div>
-            <video className="w-screen rounded-lg object-cover h-[600px]  " muted src={videoSource} loop={true} autoPlay />
+            <video className="w-screen rounded-t-3xl border border-b-0 border-yellow-500   object-cover h-[600px]  " muted src={videoSource} loop={true} autoPlay />
         </div>
     );
 };
